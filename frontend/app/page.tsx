96c2a098d3ac98cc6e67d31a1aafa3f06015d290
sakib hasnat <mdsakibkhan29@gmail.com>
2026-06-14 20:51:37 +0600

    frontend basic project setup

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+export default async function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold mb-4">Welcome to CityPolymerBD</h1>
+      <p className="text-lg text-gray-600 mb-8">
+        Your one-stop solution for polymer waste management in the city.
+      </p>
+    </div>  
+  );
+}
